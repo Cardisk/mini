15fe43bf1f83c49a11aa68dbc6bd471d7e3c1057
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2024-09-03 19:05:49 +0200

    Adding testing for reading and writing

---
+++ test/readWriteTest.cpp
@@ -0,0 +1,24 @@
+#include "gtest/gtest.h"
+
+#include "../src/mini.h"
+
+TEST(ReadWrite, WritingTest) {
+    // the path is viewed from the root of the project.
+    // change here if something is not going to work.
+    mini::Object obj("resources/testRuntime.ini");
+    bool result = obj.get_global().add_prop("testRuntime1", "value_for_testRuntime_1");
+    ASSERT_EQ(true, result);
+    result = obj.get_global().add_section("subsection");
+    ASSERT_EQ(true, result);
+    result = obj.get_section_from_path("subsection").add_prop("testRuntime2", "value_for_testRuntime_2");
+    ASSERT_EQ(true, result);
+    mini::write(obj);
+}
+
+TEST(ReadWrite, ReadingTest) {
+    // the path is viewed from the root of the project.
+    // change here if something is not going to work.
+    mini::Object obj = mini::read("resources/config.ini");
+    auto result = obj.get_prop_from_path("test1");
+    ASSERT_EQ("value_for_test_1", result);
+}
