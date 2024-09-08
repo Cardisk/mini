beb9724726e7e2776de297e17be2c7900b19a7e1
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2024-09-08 22:51:38 +0200

    Making the example more verbose

--- src/main.cpp
+++ src/main.cpp
@@ -9,22 +9,43 @@ int main(void) {
     // change here if something is not going to work.
     Object config = read("resources/config.ini");
 
-    // shortest way.
-    std::cout << config.get_prop_from_path("test1") << std::endl;
-    // longest way.
-    std::cout << config.get_global().get_prop("test1") << std::endl;
-    // shortest way.
-    std::cout << config.get_prop_from_path("subsection/test2") << std::endl;
-    // longest way.
+    std::cout << "There are 2 ways of accessing the fields:" << std::endl << std::endl;
+
+    // -------------
+    std::string test1 = "test1";
+    std::cout << "VALUE OF FIELD: " << test1 << std::endl;
+    std::cout << "\tshortest way: ";
+    std::cout << config.get_prop_from_path(test1) << std::endl;
+    std::cout << "\tlongest way: ";
+    std::cout << config.get_global().get_prop(test1) << std::endl;
+    std::cout << std::endl;
+    // -------------
+    std::string test2 = "subsection/test2";
+    std::cout << "VALUE OF FIELD: " << test2 << std::endl;
+    std::cout << "\tshortest way: ";
+    std::cout << config.get_prop_from_path(test2) << std::endl;
+    std::cout << "\tlongest way: ";
     std::cout << config.get_global().get_section("subsection").get_prop("test2") << std::endl;
-    // shortest way.
-    std::cout << config.get_section_from_path("subsection/subsubsection").get_name() << " under subsection" << std::endl;
-    // longest way.
-    std::cout << config.get_global().get_section("subsection").get_section("subsubsection").get_name() << " under subsection" << std::endl;
-    // shortest way.
-    std::cout << config.get_prop_from_path("subsection/foo/test3") << std::endl;
-    // longest way.
-    std::cout << config.get_prop_from_path("subsection/foo/test4") << std::endl;
+    std::cout << std::endl;
+    // -------------
+    std::string subsubsection = "subsection/subsubsection";
+    std::cout << "SECTION NAME OF: " << subsubsection << std::endl;
+    std::cout << "\tshortest way: ";
+    std::cout << config.get_section_from_path(subsubsection).get_name() << std::endl;
+    std::cout << "\tlongest way: ";
+    std::cout << config.get_global().get_section("subsection").get_section("subsubsection").get_name() << std::endl;
+    std::cout << std::endl;
+    // -------------
+    std::string test3 = "subsection/foo/test3";
+    std::cout << "VALUE OF FIELD: " << test3 << std::endl;
+    std::cout << "\t" << config.get_prop_from_path(test3) << std::endl;
+    std::cout << std::endl;
+    // -------------
+    std::string test4 = "subsection/foo/test4";
+    std::cout << "VALUE OF FIELD: " << test4 << std::endl;
+    std::cout << "\t" << config.get_prop_from_path(test4) << std::endl;
+    std::cout << std::endl;
+    // -------------
 
     // rewriting the same file to see the differences.
     config.set_file_path("resources/runtime.ini");
